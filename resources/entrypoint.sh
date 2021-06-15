81de2283ca6f707bcbc1c2daae600bb5d17655f0
Max Schenkelberg <max@papiocloud.com>
2021-06-15 15:55:44 -0500

    changing so only jenkins.sh runs as jenkins user #8

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
 
+function md5 {
+  md5sum "$1" | awk '{print $1}'
+}
+
+mkdir -p /var/jenkins_home/plugins
+
+# Doing this makes startup much faster, Java is much slower at this. Not 100% future proof though...
+# For each plugin, if it isn't in the plugins dir already, move it in along with expanded dir
+cd /usr/share/jenkins/ref/plugins
+
+{ time {
+for archive in `ls *.jpi`; do
+  destDir="/var/jenkins_home/plugins"
+  destFile="${destDir}/${archive}"
+  dirName=${archive%.jpi}
+  if [ ! -f $destFile ] || [ `md5 $archive` != `md5 $destFile` ]; then
+    echo "Synchronizing plugin ${archive}"...
+    rsync -az $archive "${destDir}/"
+    rsync -az --delete $dirName "${destDir}/"
+  fi
+  # Move files so jenkins start script doesn't attempt to copy
+  rm -rf $archive $dirName
+done
+} }
+
+cd -
+
+# Ensure proper ownership of all /var/jenkins_home
 # TODO: Support configurable jenkins user/home
 chown jenkins:jenkins /var
 chown -R jenkins:jenkins /var/jenkins_home
 
-# Run the custom startup script as jenkins user
-su -c "/resources/startup.sh" -m jenkins
+# Run jenkins.sh as the jenkins user
+su -c "/usr/local/bin/jenkins.sh" -m jenkins
